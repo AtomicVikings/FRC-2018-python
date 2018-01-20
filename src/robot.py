dc80062610bb65bdbdd8a28f1a246dfeb8ea8375
upexus <pixleylogan@gmail.com>
2018-01-19 20:08:43 -0500

    we created a basic framework for the FRC 2018 bot program

--- src/robot.py
+++ src/robot.py
@@ -1,2 +1,35 @@
 import wpilib
+import ctre
+
 from wpilib import RobotDrive
+
+class MyRobot(wpilib.IterativeRobot):
+	#Motors canbus
+	Left_Motor = 1
+	Right_Motor = 2
+	Front_Intake = 3
+	
+	#Joystick
+	Joystick_Channel = 0
+    def robotInit(self):
+		self.F_Intake = ctre.WPI_TalonSRX(Front_Intake)
+		self.Joystick = wpilib.Joystick(Joystick_Channel)
+		
+	def disabledPeriodic(self):
+		self.F_Intake.disable()
+    
+    def autonomousPeriodic(self):
+        pass
+    
+    def teleopInit(self):
+        pass
+    
+    def teleopPeriodic(self):
+		if Joystick.getButton(1):
+			self.F_Intake.set(0.5)
+		else:
+			self.F_Intake.set(0)
+		
+    
+if __name__ == '__main__':
+    wpilib.run(MyRobot)
