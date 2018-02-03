d4afa8a2f0737ad46c7ea78a3378283fc50d37f1
upexus <pixleylogan@gmail.com>
2018-02-03 11:58:18 -0500

    make it drive

--- src/robot.py
+++ src/robot.py
@@ -1,34 +1,58 @@
 import wpilib
-import ctre
+#import ctre
 
-from wpilib import RobotDrive
+from wpilib.drive import DifferentialDrive
 
 class MyRobot(wpilib.IterativeRobot):
 	#Motors canbus
-	Left_Motor = 1
-	Right_Motor = 2
+	Left_Motor_Front 	= 8
+	Right_Motor_Front 	= 7
+	Left_Motor_Back 	= 9
+	Right_Motor_Back 	= 6
+	
 	Front_Intake = 3
 	
 	#Joystick
 	Joystick_Channel = 0
-    def robotInit(self):
-		self.F_Intake = ctre.WPI_TalonSRX(Front_Intake)
-		self.Joystick = wpilib.Joystick(Joystick_Channel)
+	def robotInit(self):
+		#Mechanisms
+		#self.F_Intake = ctre.WPI_TalonSRX(self.Front_Intake)
 		
+		#Joysticks
+		self.Joystick = wpilib.Joystick(self.Joystick_Channel)
+		
+		#Drive Motors
+		self.LMF = wpilib.Talon(self.Left_Motor_Front) 
+		self.RMF = wpilib.Talon(self.Right_Motor_Front)
+		self.LMB = wpilib.Talon(self.Left_Motor_Back)
+		self.RMB = wpilib.Talon(self.Right_Motor_Back)
+
+		self.Left = wpilib.SpeedControllerGroup(self.LMF, self.LMB)
+		self.Right = wpilib.SpeedControllerGroup(self.RMF, self.RMB)
+        
+		#This is not the built-in robot drive 
+		self.My_Robot = DifferentialDrive(self.Left, self.Right)
+		self.My_Robot.setExpiration(0.1)
+        
+	
 	def disabledPeriodic(self):
-		self.F_Intake.disable()
+		pass
+		#self.F_Intake.disable()
     
-    def autonomousPeriodic(self):
-        pass
+	def autonomousPeriodic(self):
+		pass
     
-    def teleopInit(self):
-        pass
-    
-    def teleopPeriodic(self):
-		if Joystick.getButton(1):
-			self.F_Intake.set(0.5)
-		else:
-			self.F_Intake.set(0)
+	def teleopInit(self):
+		self.My_Robot.setSafetyEnabled(True)
+		
+	#The function will get called around 50 times a second	
+	def teleopPeriodic(self):
+		self.My_Robot.tankDrive(self.Joystick.getY() * -1, self.Joystick.getY() * -1)
+		
+		#if self.Joystick.getButton(1):
+		#	self.F_Intake.set(0.5)
+		#else:
+		#	self.F_Intake.set(0)
 		
     
 if __name__ == '__main__':
